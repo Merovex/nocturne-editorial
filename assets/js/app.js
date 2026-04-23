0fafdd7b1f3d64406f7a63d02c2f245fd20c1c56
Ben W <merovex@hey.com>
2026-04-23 14:22:48 -0400

    register magnet-filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ import ThemeController from '/assets/js/controllers/theme_controller.js';
 import NavController from '/assets/js/controllers/nav_controller.js';
 import NewsletterController from '/assets/js/controllers/newsletter_controller.js';
 import ScrollCtaController from '/assets/js/controllers/scroll_cta_controller.js';
+import MagnetFilterController from '/assets/js/controllers/magnet_filter_controller.js';
 
 window.Stimulus = Application.start();
 
@@ -10,3 +11,4 @@ window.Stimulus.register("theme", ThemeController);
 window.Stimulus.register("nav", NavController);
 window.Stimulus.register("newsletter", NewsletterController);
 window.Stimulus.register("scroll-cta", ScrollCtaController);
+window.Stimulus.register("magnet-filter", MagnetFilterController);

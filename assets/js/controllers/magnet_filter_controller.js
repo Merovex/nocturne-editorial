e4570f62074a1c941faf5655ad1d24d5a3925a19
Ben W <merovex@hey.com>
2026-04-23 14:07:31 -0400

    Implement magnet filtering by audience tag in squeeze layout

---
+++ assets/js/controllers/magnet_filter_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "../stimulus.min.js"
+
+export default class extends Controller {
+  static targets = ["magnet"]
+
+  connect() {
+    const audience = new URLSearchParams(window.location.search).get("audience")
+    if (!audience) return
+    this.magnetTargets.forEach(el => {
+      const tag = el.dataset.audienceTag
+      if (tag && tag !== audience) el.hidden = true
+    })
+  }
+}
